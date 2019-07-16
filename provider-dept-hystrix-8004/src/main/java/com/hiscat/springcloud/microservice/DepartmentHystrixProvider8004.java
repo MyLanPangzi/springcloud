6380a3836bf0602ea64145f1e47366a1f0953cab
MyLanpangzi <1251723871@qq.com>
2019-07-16 19:06:15 +0800

    feat(all): add hystrix service down level

--- provider-dept-hystrix-8004/src/main/java/com/hiscat/springcloud/microservice/DepartmentHystrixProvider8004.java
+++ provider-dept-hystrix-8004/src/main/java/com/hiscat/springcloud/microservice/DepartmentHystrixProvider8004.java
@@ -2,12 +2,12 @@ package com.hiscat.springcloud.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @EnableDiscoveryClient
+@EnableHystrix
 public class DepartmentHystrixProvider8004 {
     public static void main(String[] args) {
         SpringApplication.run(DepartmentHystrixProvider8004.class, args);
